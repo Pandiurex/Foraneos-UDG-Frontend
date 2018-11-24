31ab215b0558d51aab6a1a3e3305fcaef417de6f
Cristopher Estrada <crissin21_01@hotmail.com>
2018-11-24 15:18:18 -0600

    menu de lugares fue creado, se incluyo un script para gulp y ya se automatizan ciertas tareas

---
+++ app/js/slider.js
@@ -0,0 +1,53 @@
+class Slider{
+  constructor(sliderId){
+    this.sliderImages = document.querySelectorAll(`#${sliderId} .slide`);
+    this.arrowLeft = document.querySelector(`#${sliderId} .arrow-left`);
+    this.arrowRight = document.querySelector(`#${sliderId} .arrow-right`);
+    this.current = 0;
+    self = this;
+    }
+  reset(){
+    for(let i = 0; i < this.sliderImages.length; i++){
+      this.sliderImages[i].style.display = 'none';
+    }
+  }
+
+  startSlide(){
+    this.reset();
+    this.sliderImages[0].style.display = 'block';
+
+    this.arrowLeft.addEventListener('click', function(){
+      if(self.current === 0){
+        self.current = self.sliderImages.length;
+      }
+      self.slideLeft();
+    });
+
+    this.arrowRight.addEventListener('click', function(){
+      if(self.current === self.sliderImages.length - 1){
+        self.current = -1
+      }
+      self.slideRight();
+    }, false);
+
+    setInterval(() => {
+      self.arrowRight.click();
+    }, 5000);
+  }
+
+  slideLeft(){
+    this.reset();
+    this.sliderImages[this.current - 1].style.display = 'block';
+    this.current--;
+  }
+
+  slideRight() {
+    this.reset();
+    this.sliderImages[this.current + 1].style.display = 'block';
+    this.current++;
+  }
+
+
+}
+let sliderPrincipal = new Slider('sliderPrincipal');
+  sliderPrincipal.startSlide();
