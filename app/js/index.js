4324de1fc7c1d88b946abea3880f4fab9e8608c9
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-04 20:53:43 -0600

    Index organizado

--- app/js/index.js
+++ app/js/index.js
@@ -1,31 +1,24 @@
+const login = document.getElementById('input_login');
 
-var login = document.getElementById('input_login');
-
-login.addEventListener('submit', function(e){
+login.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('Click')
+  console.log('Click');
 
-  var datos = new FormData(login);
-  const data = new URLSearchParams(new FormData(login));
+  const datos = new FormData(login);
+  const datas = new URLSearchParams(new FormData(login));
   const params = new URLSearchParams([...new FormData(e.target).entries()]);
-  console.log(datos)
-  console.log(datos.get('email'))
-  console.log(datos.get('password'))
-  console.log(data.get('email'))
-  console.log(data.get('password'))
-  console.log(params.get('email'))
-  console.log(params.get('password'))
+  console.log(datos);
+  console.log(datos.get('email'));
+  console.log(datos.get('password'));
 
-  fetch('https://api.foraneos-udg.ml/api/auth/login',{
+  fetch('https://api.foraneos-udg.ml/api/auth/login', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: new URLSearchParams( datos.get('email'), datos.get('password') )
+    body: new URLSearchParams(datos.get('email'), datos.get('password')),
   })
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch( e => console.error('Error') )
-})
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+});
