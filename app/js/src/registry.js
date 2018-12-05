e706abaed9a56228303a9ed9eb54e8e89e9b80ed
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-05 14:13:46 -0600

    Correcciones

--- app/js/src/registry.js
+++ app/js/src/registry.js
@@ -1,12 +1,6 @@
 import regexs from '../util/regexs.js';
 
-window.onload = start;
-
 class Registry {
-  constructor() {
-    this.form = document.getElementById('register-form');
-  }
-
   checkForm() {
     this.getElements();
     this.clearElements();
@@ -15,11 +9,13 @@ class Registry {
 
   getElements() {
     this.elements = [];
-    this.elements.name = this.form.name;
-    this.elements.lastname = this.form.lastname;
-    this.elements.email = this.form.regemail;
-    this.elements.password = this.form.regpassword;
-    this.elements.confirmpassword = this.form.confirmpass;
+    this.elements.name = document.getElementById('name');
+    this.elements.firstSurname = document.getElementById('firstsurname');
+    this.elements.secondSurname = document.getElementById('secondsurname');
+    this.elements.email = document.getElementById('regemail');
+    this.elements.password = document.getElementById('regpassword');
+    this.elements.confirmPassword = document.getElementById('confirmpass');
+    console.log(this.elements);
   }
 
   clearElements() {
@@ -32,13 +28,13 @@ class Registry {
     let correct = true;
     Object.values(this.elements).forEach((element) => {
       if (element.selectedIndex !== undefined) {
-        if (element.selectedIndex === 0 && element.dataset.required) {
+        if (element.selectedIndex === 0 && element.required) {
           this.markElement(element);
           correct = false;
         }
         return;
       }
-      if (element.dataset.required && element.value.length === 0) {
+      if (element.required && element.value.length === 0) {
         this.markElement(element);
         correct = false;
       } else if (element.value.length !== 0) {
@@ -48,11 +44,6 @@ class Registry {
         }
       }
     });
-    
-    if (this.form.regpassword.value !== this.form.confirmpass.value) {
-      this.markElement(this.form.regpassword);
-      this.markElement(this.form.confirmpass);
-    }
 
     if (correct) {
       // enviar los VALORES al modelo
@@ -71,15 +62,7 @@ class Registry {
   }
 }
 
-function processRegisterForm() {
+document.getElementById('btnregister').addEventListener('click', () => {
   const registry = new Registry();
   registry.checkForm();
-}
-
-function start() {
-  document.getElementById('register-form')
-    .addEventListener('submit', (e) => {
-      e.preventDefault();
-      processRegisterForm();
-    }, false);
-}
+});
