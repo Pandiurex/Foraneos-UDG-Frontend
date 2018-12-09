12ec1092b7809bfb076b6177b83d449b3d4b3366
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-09 17:34:41 -0600

    Obtencion de imagenes

--- app/users/profile/locations/new/src/index.js
+++ app/users/profile/locations/new/src/index.js
@@ -4,21 +4,28 @@ import regexs from '../../../../../js/util/regexs.js';
 import { checkRequired } from '../../../../../js/util/validator.js'
 import { getKeyValues, clearUndefined } from '../../../../../js/util/list.js';
 import { hideElements } from '../../../../../js/util/hideElements.js';
+import goTo from '../../../../../js/util/goTo.js';
 
 window.addEventListener('load', start);
+
+const images = [];
 
 function start() {
   const type = Cookie.getCookie('type');
   console.log(type);
   hideElements(type);
 
-  if (type === '1') {
-    window.location.replace("./index.html");
+  if (type !== '1') {
+    goTo("/");
   }
 }
 
 document.getElementById('btngua').addEventListener('click', () => {
   checkForm();
+});
+
+document.getElementById('file-upload').addEventListener('change', (event) => {
+  pushImage(event);
 });
 
 function checkForm() {
@@ -49,4 +56,93 @@ function getElements() {
   elements.commentsElement = document.getElementById('comentarios');
   elements.restrictionsElement = document.getElementById('restricciones');
   return elements;
+}
+
+function pushImage(event) {
+  images.push(event.target.files[0]);
+  console.log(images);
+
+  if (images.length === 5) {
+    deleteInputFile();
+  } else {
+    recreateInputFile();
+  }
+
+  createModal(images.length, images[images.length - 1]);
+  createImgRef(images.length);
+}
+
+function recreateInputFile() {
+  const inputFile = document.getElementById('file-upload');
+  const clone = inputFile.cloneNode();
+  clone.value = '';
+  inputFile.parentNode.replaceChild(clone, inputFile);
+
+  clone.addEventListener('change', (event) => {
+    pushImage(event);
+  });
+}
+
+function deleteInputFile() {
+  const inputFile = document.getElementById('file-upload');
+  inputFile.parentNode.innerHTML = '';
+}
+
+function createImgRef(id) {
+  const allImages = document.getElementById('all-images');
+  const li = document.createElement('li');
+  const aImg = document.createElement('a');
+  aImg.href = `#img${id}`;
+  const textImg = document.createTextNode(`Ver foto ${id}`);
+
+  allImages.appendChild(li);
+  li.appendChild(aImg);
+  aImg.appendChild(textImg);
+}
+
+function createModal(id, image) {
+  let leftRef = '';
+  let rightRef = '';
+  if (id === 0) {
+    leftRef = `#img${images.length - 1}`;
+  }
+  if (id === images.length - 1) {
+    rightRef = '#img0';
+  }
+
+  const fotoModals = document.getElementById('foto-modals');
+  const divModalimg = document.createElement('div');
+  divModalimg.className = 'modalimg';
+  divModalimg.id = `img${id}`;
+  const h4 = document.createElement('h4');
+  const textFoto = document.createTextNode(`Foto ${id}`);
+  const divImagen = document.createElement('div');
+  divImagen.className = 'imagen';
+  const aLeft = document.createElement('a');
+  aLeft.href = leftRef
+  const textLeft = document.createTextNode('<');
+  const aEmpty = document.createElement('a');
+  const img = document.createElement('img');
+  img.src = URL.createObjectURL(image);
+  img.alt = `Foto ${id}`;
+  const aRight =  document.createElement('a');
+  aRight.href = rightRef;
+  const textRight = document.createTextNode('>');
+  const aCerrar = document.createElement('a');
+  aCerrar.className = 'cerrar';
+  aCerrar.href = '#';
+  const textCerrar = document.createTextNode('X');
+
+  fotoModals.appendChild(divModalimg);
+  divModalimg.appendChild(h4);
+  divModalimg.appendChild(divImagen);
+  divModalimg.appendChild(aCerrar);
+  h4.appendChild(textFoto);
+  divImagen.appendChild(aLeft);
+  divImagen.appendChild(aEmpty);
+  divImagen.appendChild(aRight);
+  aLeft.appendChild(textLeft);
+  aEmpty.appendChild(img);
+  aRight.appendChild(textRight);
+  aCerrar.appendChild(textCerrar);
 }
